1c1b1170c9eb474ae3bf5cee24e48fef9582a55b
williamwu971 <85791955+williamwu971@users.noreply.github.com>
2022-05-24 19:57:52 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -6,7 +6,9 @@ sudo snap install clion --classic
 sudo snap install pycharm-professional --classic
 
 sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison \
-clang libopenmpi-dev python-is-python3 python3-pip
+clang libopenmpi-dev python-is-python3 python3-pip apt-file
+
+sudo apt-file update
 
 git config --global user.email "xiwu9085@gmail.com"
 git config --global user.name "William"
