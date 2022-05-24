67a270c74b9513d6de98b324b867175a8055408c
williamwu971 <85791955+williamwu971@users.noreply.github.com>
2022-05-24 06:48:02 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,17 +2,11 @@
 
 echo "compile perf manually: https://github.com/torvalds/linux.git"
 
-sudo snap refresh
 sudo snap install clion --classic
 sudo snap install pycharm-professional --classic
 
-sudo apt update
-sudo apt upgrade
-
 sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison \
   clang libopenmpi-dev python-is-python3 python3-pip apt-file nvidia-cuda-dev wget gcc g++ gdb
-
-sudo apt-file update
 
 git config --global user.email "xiwu9085@gmail.com"
 git config --global user.name "William"
