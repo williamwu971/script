51d6e2185f85d33516eaf4157f8e835383b88d6f
William <xiwu9085@gmail.com>
2022-05-24 08:29:15 -0400

    more

--- install.bash
+++ install.bash
@@ -2,14 +2,17 @@
 
 echo "compile perf manually: https://github.com/torvalds/linux.git"
 
+sudo snap refresh
 sudo snap install clion --classic
 sudo snap install pycharm-professional --classic
 
 sudo apt update
+sudo apt upgrade
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo ubuntu-drivers autoinstall
 sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison \
-  clang libopenmpi-dev python-is-python3 python3-pip apt-file nvidia-cuda-dev wget gcc g++ gdb libglvnd-dev pkg-config
+  clang libopenmpi-dev python-is-python3 python3-pip apt-file nvidia-cuda-dev wget gcc g++ gdb \
+  libglvnd-dev pkg-config
 
 git config --global user.email "xiwu9085@gmail.com"
 git config --global user.name "William"
