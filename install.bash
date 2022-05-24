72520884bb74918062a6458dcb23af56f01fd317
williamwu971 <85791955+williamwu971@users.noreply.github.com>
2022-05-24 19:31:13 +1000

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,9 @@
+
+
+sudo snap install clion --classic
+sudo snap install pycharm-professional --classic
+sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+git config --global user.email "xiwu9085@gmail.com"
+git config --global user.name "William"
+git config --global credential.helper store
+
