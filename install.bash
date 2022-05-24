0010e63e5da6ecd07bb41fc7f3098d785d5c179a
williamwu971 <85791955+williamwu971@users.noreply.github.com>
2022-05-24 19:48:48 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ echo "kernel: https://github.com/torvalds/linux.git"
 sudo snap install clion --classic
 sudo snap install pycharm-professional --classic
 
-sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison clang
+sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison clang libopenmpi-dev
 
 git config --global user.email "xiwu9085@gmail.com"
 git config --global user.name "William"
