27b0d1b583f0af99ec8e127feb5375e2e376e165
williamwu971 <85791955+williamwu971@users.noreply.github.com>
2022-05-24 19:40:10 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,10 +2,13 @@
 
 sudo snap install clion --classic
 sudo snap install pycharm-professional --classic
+
 sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+
 git config --global user.email "xiwu9085@gmail.com"
 git config --global user.name "William"
 git config --global credential.helper store
+
 sed -i 's/@\\h//g' ~/.bashrc
 sed -i 's/\\w/\\W/g' ~/.bashrc
 
